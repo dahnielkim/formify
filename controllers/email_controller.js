3d69513386589e2884c465bc8cccc4b304c74b9a
Terrence <terrence@multifold.com>
2019-07-22 09:41:25 -0700

    add data parser to email controller, wip

--- controllers/email_controller.js
+++ controllers/email_controller.js
@@ -1,12 +1,42 @@
 const nodemailer = require('nodemailer');
 
 //'smtps://[email_address]%40gmail.com:[password]@smtp.gmail.com'
-var transporter = nodemailer.createTransport('smtps://mlaw8788%40gmail.com:tbone2888@smtp.gmail.com');
+let transporter = nodemailer.createTransport('smtps://mlaw8788%40gmail.com:tbone2888@smtp.gmail.com');
+
+function parseData(_data) {
+    let arrCsv = [];
+    let result = {};
+    let commonData = [];
+    Object.keys(_data).forEach(key => {
+        let value = _data[key];
+        if(!Array.isArray(value)) {
+            commonData.push(value);
+        }
+    })
+
+    Object.keys(_data).forEach(key => {
+        let value = _data[key];
+        if(Array.isArray(value)) {
+            value.forEach(obj => {
+                let chunk = [];
+                Object.keys(obj).forEach(key => {
+                    let attribute = obj[key];
+                    chunk.push(attribute);
+                })
+                arrCsv.push(chunk.concat(commonData).join(','));
+            })
+            result[key] = arrCsv.join('\r\n')
+            arrCsv = [];
+        }
+    })
+    return result;
+}
 
 module.exports = {
     sendEmail(req, res, next) {
         let data = req.body;
-        console.log(data);
+        let body = parseData(data);
+        console.log('body', body);
 
         var mailOptions = {
         from: '"mlaw" <mlaw8788@gmail.com>',
@@ -26,6 +56,6 @@ module.exports = {
       // });
 
 
-      // res.send(data)
+      res.send(data)
     }
 }
