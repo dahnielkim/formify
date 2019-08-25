9bb3552ee31cdde2ffbaf3655d38f8cab6a593ee
Daniel Kim <dkim@vlocity.com>
2019-08-25 02:08:55 -0700

    Reformatted

--- src/components/TableBody/TableBody.js
+++ src/components/TableBody/TableBody.js
@@ -2,13 +2,7 @@ import React from 'react';
 import TableRowInput from '../DynamicInput';
 
 const TableBody = props => (
-    <TableRowInput
-        invoice={props.data}
-        type={props.typeAbbrev}
-        keyValue={props.typeLong}
-        handleChange={props.fn}
-        handleRemove={props.deleteFn}
-    />
+    <TableRowInput invoice={props.data} type={props.typeAbbrev} keyValue={props.typeLong} handleChange={props.fn} handleRemove={props.deleteFn} />
 );
 
 export default TableBody;
