84d25b79548b30ac8212f025094c004c8fd370be
Daniel Kim <dkim@vlocity.com>
2019-09-08 09:50:44 -0700

    #11 - fixed build script

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const bodyParser = require('body-parser');
+const EmailController = require('./controllers/email_controller');
+
+const isDev = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 5000;
+
+// Multi-process to utilize all CPU cores.
+if (!isDev && cluster.isMaster) {
+    console.error(`Node cluster master ${process.pid} is running`);
+
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+    });
+} else {
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.text());
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+    app.use((err, req, res, next) => {
+        res.status(422).send({ errors: err.message });
+    });
+
+    // priority serve any static files.
+    app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+    app.get('/', (req, res) => res.send('Hello World!'));
+    app.post('/api/email', EmailController.sendEmail);
+    app.get('/api', function(req, res) {
+        res.set('Content-Type', 'application/json');
+        res.send('{"message":"Hello from the custom server!"}');
+    });
+
+    app.get('*', function(request, response) {
+        response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    });
+
+    app.listen(PORT, function() {
+        console.error(`Node ${isDev ? 'dev server' : 'cluster worker ' + process.pid}: listening on port ${PORT}`);
+    });
+}
