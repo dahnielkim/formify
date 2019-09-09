9091abc1b7a10ec5c486ec00aae42cf55f678a7f
Daniel Kim <dkim@vlocity.com>
2019-09-09 14:47:59 -0700

    updated the request email body data

--- controllers/form.js
+++ controllers/form.js
@@ -22,23 +22,23 @@ module.exports = {
 
         if (req.body.id) {
             for (let i = 0; i < req.body.unpaidInvoices.length; i++) {
-                emailBody += 'report_id|' + req.body.id + '|';
-                emailBody += 'vendor_name|' + req.body.vendor_name + '|';
-                emailBody += 'vendor_email|' + req.body.email + '|';
-                emailBody += 'type|unpaid_invoices|';
-                emailBody += 'description|' + req.body.unpaidInvoices[i].description + '|';
-                emailBody += 'amount|' + req.body.unpaidInvoices[i].amount + '|';
-                emailBody += 'period|' + req.body.unpaidInvoices[i].period + '\n';
+                emailBody += req.body.id + '|';
+                emailBody += req.body.vendor_name + '|';
+                emailBody += req.body.email + '|';
+                emailBody += 'unpaid_invoices|';
+                emailBody += req.body.unpaidInvoices[i].description + '|';
+                emailBody += req.body.unpaidInvoices[i].amount + '|';
+                emailBody += req.body.unpaidInvoices[i].period + '\n';
             }
 
             for (let i = 0; i < req.body.uninvoicedInvoices.length; i++) {
-                emailBody += 'report_id|' + req.body.id + '|';
-                emailBody += 'vendor_name|' + req.body.vendor_name + '|';
-                emailBody += 'vendor_email|' + req.body.email + '|';
-                emailBody += 'type|uninvoiced_invoices|';
-                emailBody += 'description|' + req.body.uninvoicedInvoices[i].description + '|';
-                emailBody += 'amount|' + req.body.uninvoicedInvoices[i].amount + '|';
-                emailBody += 'period|' + req.body.uninvoicedInvoices[i].period + '\n';
+                emailBody += req.body.id + '|';
+                emailBody += req.body.vendor_name + '|';
+                emailBody += req.body.email + '|';
+                emailBody += 'uninvoiced_invoices|';
+                emailBody += req.body.uninvoicedInvoices[i].description + '|';
+                emailBody += req.body.uninvoicedInvoices[i].amount + '|';
+                emailBody += req.body.uninvoicedInvoices[i].period + '\n';
             }
 
             const mailOptions = {
